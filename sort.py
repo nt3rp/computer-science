6c1c9799d191c823f1718c6dc483ca86d4d27fc0
Nicholas Terwoord <nicholas.terwoord+code@gmail.com>
2014-02-28 00:04:39 -0500

    Add selection sort implementation
    - Also, erroneously revised quicksort description

--- sort.py
+++ sort.py
@@ -1,4 +1,9 @@
 def quick_sort(items):
+    # This implementation will use more memory than necessary
+    # as we create new lists. We could *not* do this, but the below
+    # is pretty simple code.
+
+    # TODO: Do this creating as few lists as possible.
     n = len(items)
 
     # Base case
@@ -26,11 +31,6 @@ def merge(left, right):
 
 
 def merge_sort(items):
-    """
-    Divide the set into two groups, until there are only two elements.
-
-    Then, sort on the way up.
-    """
     n = len(items)
 
     # Base case
@@ -45,14 +45,25 @@ def merge_sort(items):
     # Merge
     return merge(left, right)
 
-def selection_sort():
+def selection_sort(items):
     """
     Iterate through a list to find the smallest element.
 
     Then, find the next small it and insert it at the correct location.
     And so on.
     """
-    pass
+
+    results = []
+
+    while items:
+        min_v = None
+        for item in items:
+            if min_v is None or item < min_v:
+                min_v = item
+        results.append(min_v)
+        items.remove(min_v)
+
+    return results
 
 def insertion_sort():
     """
